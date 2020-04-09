318e7f77c76b82a9537d45b89e5cf09c894cf480
Aashis Kumar <46660177+ak799@users.noreply.github.com>
2020-04-09 12:17:00 +0530

    Create Variable Assignment.py

---
+++ Python Basics/Variable Assignment.py
@@ -0,0 +1,20 @@
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print (savings)
+
+
+Calculations with variables
+
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = savings * growth_multiplier ** 7
+
+# Print out result
+print(result)
